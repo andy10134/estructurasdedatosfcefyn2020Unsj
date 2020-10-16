7c052e1486324cf0784ca9034401e52c6e7536c4
andy10134 <andy10134@hotmail.com>
2020-10-16 16:23:07 -0300

    ejercicio 1 a listo

--- Practica Lista/Ejercicio 1/Lista-Secuencial/Lista.cpp
+++ Practica Lista/Ejercicio 1/Lista-Secuencial/Lista.cpp
@@ -11,6 +11,7 @@ using namespace std;
     this -> tope = -1;
 }
 
+//Vacia
 int Lista::vacia(void)
 {
     return( this -> tope == -1);
@@ -20,8 +21,9 @@ int Lista::vacia(void)
 int Lista::insertar(int elemento, int posicion)
 {
     int i, aux = 0;
+    posicion -= 1;
 
-    if ( this -> cantidadMax > this -> tope )
+    if ( this -> cantidadMax - 1 > this -> tope && posicion >= 0 )
     {
         if ( posicion - 1 <= this -> tope )
         {
@@ -48,8 +50,9 @@ int Lista::suprimir(int posicion)
 {
     int i, aux = 0;
 
-    if( posicion - 1 <= this -> tope )
+    if( posicion > 0 && posicion - 1 <= this -> tope )
     {
+        posicion -= 1;
         aux = this -> items[ posicion ];
 
         for ( i = posicion; i <= this -> tope; i++)
@@ -64,8 +67,89 @@ int Lista::suprimir(int posicion)
     return(aux);
 }
 
+//recuperar
+int Lista::recuperar(int posicion)
+{
+    int aux;
+    posicion -= 1;
+    if( posicion >= 0 && this -> tope >= posicion )
+    {
+        aux = this -> items[ posicion ];
+    }
 
-void Lista::mostrar(void)
+    return(aux);
+}
+
+//buscar
+int Lista::buscar(int elemento)
+{
+    int i = 0, bandera = 0;
+
+    while ( i <= this -> tope && !bandera )
+    {
+        if( elemento == this -> items[ i ] )
+        {
+            bandera = 1;
+        }else{
+            i++;
+        }
+    }
+
+    if( i <= this -> tope )
+    {
+        return(i+1);
+    }else
+    {
+        return(-1);
+    }
+    
+    
+}
+
+//Devuelve el primer elemento
+int Lista::primerElemento(void)
+{
+    return( this -> items[ 0 ] );
+}
+
+//Devuelve el ultimo elemento
+int Lista::ultimoElemento(void)
+{
+    return( this -> items[ this -> tope ] );
+}
+
+//Suiguiente elemento de la posicion
+int Lista::siguiente(int posicion)
+{
+    int aux = -1;
+    if( posicion < this -> tope )
+    {
+        aux = this -> items[ posicion + 1 ];
+    }else
+    {
+        cout << "No existe posicion siguiente..." << endl; 
+    }
+
+    return(aux);    
+}
+
+//Anterior elemento de la posicion
+int Lista::anterior(int posicion)
+{
+    int aux = -1;
+    if( posicion > 0 )
+    {
+        aux = this -> items[ posicion - 1 ];
+    }else
+    {
+        cout << "No existe posicion anterior..." << endl; 
+    }
+
+    return(aux);    
+}
+
+//Muestra lista
+void Lista::recorrer(void)
 { 	
     int i;
     for ( i = 0; i <= this -> tope ; i++)
@@ -74,16 +158,3 @@ void Lista::mostrar(void)
     }
     
 }
-
-  
-// INTENTO DE CARGA  
-/*    if( this -> tope < this -> cantidadMax)
-    {
-        if( posicion -1 == this -> tope)
-        {
-            cout << "xd" << endl;
-            this -> items[posicion] = elemento;
-            this -> tope ++;
-        }
-    }    
-    */
