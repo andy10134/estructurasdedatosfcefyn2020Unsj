b8d01d29b2f48e92cab7af98c923e28dd28365f0
AnaCarbajo <aniic.1701@gmail.com>
2020-10-23 15:56:39 -0300

    .

--- Practica Lista/Ejercicio 4/main.cpp
+++ Practica Lista/Ejercicio 4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string.h>
 #include "designacion.cpp"
 #include "ListaEnlazada.cpp"
@@ -10,12 +11,23 @@ main()
 {
     ifstream archivo; 
     string linea, cargo, instancia, materia;
-    char* linea2, *dato;
-    int anio, varones, mujeres;
+    char* linea2;
+    string arreglo[5];
+    std::string dato;
+    int anio, varones, mujeres, i;
     ListaEnlazada lista = ListaEnlazada(0);
     archivo.open("estadistica-designacion-magistrados-federal-nacional-por-genero-20190913.csv", ios::in);
+    getline(archivo,linea); // lee la primera fila con los nombres
     while (getline(archivo,linea))
     {
+        /*i = 0; 
+        stringstream stream(linea);
+        while(getline(stream, dato, ','))
+        {
+            arreglo[i] = dato;
+            i++;
+        }
+        Designacion desig = Designacion(std::stoi(arreglo[0]), arreglo[1], arreglo[2], arreglo[3], std::stoi(arreglo[4]), std::stoi(arreglo[5]));*/
         linea2 = (char *)linea.c_str();
         dato = strtok(linea2, ",");
         anio = atoi(dato);
