e3ea3d9289b84bc9515c3f5590eff3fbb4ddbbf0
AnaCarbajo <aniic.1701@gmail.com>
2020-10-29 13:09:07 -0300

    .

--- Practica Cola-Pila/Ejercicio 7/main.cpp
+++ Practica Cola-Pila/Ejercicio 7/main.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
 #include "ColaEnlazada.cpp"
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
 
 int main()
 {
-    int frecCliente, atencionCaj, tiempoEspera = 0, cajero = 0, max = 0, reloj = 0, TMS = 90;
+    int frecCliente = 2, atencionCaj = 5, tiempoEspera = 0, cajero = 0, max = 0, reloj = 0, TMS = 90;
     ColaEnlazada cola = ColaEnlazada(0,0,0);
-    cout<<"Ingrese tiempo de atencion de cajero"<<endl;
+    /*cout<<"Ingrese tiempo de atencion de cajero"<<endl;
     cin>>atencionCaj;
     cout<<"Ingrese frecuencia de llegada de los clientes"<<endl;
-    cin>>frecCliente;
+    cin>>frecCliente;*/
+    srand(time(NULL));
     while (reloj < TMS)
     {
-        float random = (float) (rand()%11);
+        /*float random = (float) (rand()%11);
         random = random/10;
-        float division = (float) 1/frecCliente;
-        if ( random == division)
+        float division = (float) 1/frecCliente;*/
+        int random = rand()%5;
+        int division = 1;
+        if ( random == division) //probabilidad de que llegue alguien a la cola
         {
             cola.insertar(reloj);
+            cout<<"entro alguien a la cola"<<endl;
         }
         if(cajero == 0)
         {
             if (! cola.vacia())
             {
                 tiempoEspera = cola.suprimir();
-                tiempoEspera = reloj - tiempoEspera;
+                tiempoEspera = reloj - tiempoEspera; //calcula cuanto tiempo espero el chabon en la cola
                 if (tiempoEspera > max)
                 {
-                    max = tiempoEspera;
+                    max = tiempoEspera; //se guarda el max de tiempo de espera
                 }
-                cajero = atencionCaj;
+                cajero = atencionCaj; //iniciar cajero en 5
+                cout<<"el cajero atendio a alguien"<<endl;
             }
             
         }
--- Practica Lista/Ejercicio 3/Ejercicio 3/Ejercicio 3.cpp
+++ Practica Lista/Ejercicio 3/Ejercicio 3/Ejercicio 3.cpp
@@ -8,50 +8,50 @@ using namespace std;
 
 main()
 {
-Lista lista = Lista();
-ifstream archivo; 
-string linea, provincia;
-int i=-2, j=0, a;
-float sumatoria=0, num;
-string A[200][15];
-archivo.open("forestales.csv", ios::in);
-while (getline(archivo, linea))
-{
-    string columna;
-    stringstream strstr(linea); // Convierte a string la linea obtenida
-    i++;
-    if (i != -1)
-    {
-        while (getline(strstr, columna, ';'))
-        {
-            A[i][j] = columna; 
-            j++;
-        }
-    }
-    j = 0;
-}
-i++;
-provincia=A[0][3];
-for (a=0; a<i; a++)
-{
-	if ( provincia == A[a][3] )
+	Lista lista = Lista();
+	ifstream archivo; 
+	string linea, provincia;
+	int i=-2, j=0, a;
+	float sumatoria=0, num;
+	string A[200][15];
+	archivo.open("forestales.csv", ios::in);
+	while (getline(archivo, linea))
 	{
-		stringstream floatValue(A[a][6]);
-		num = 0;
-		floatValue >> num;
-		sumatoria += num;
+		string columna;
+		stringstream strstr(linea); // Convierte a string la linea obtenida
+		i++;
+		if (i != -1)
+		{
+			while (getline(strstr, columna, ';'))
+			{
+				A[i][j] = columna; 
+				j++;
+			}
+		}
+		j = 0;
 	}
-	else
+	i++;
+	provincia=A[0][3];
+	for (a=0; a<i; a++)
 	{
-		lista.insertar(sumatoria, provincia);
-		sumatoria = 0;
-		stringstream floatValue(A[a][6]);
-		num = 0;
-		floatValue >> num;
-		sumatoria += num;
-		provincia = A[a][3];
+		if ( provincia == A[a][3] )
+		{
+			stringstream floatValue(A[a][6]);
+			num = 0;
+			floatValue >> num;
+			sumatoria += num;
+		}
+		else
+		{
+			lista.insertar(sumatoria, provincia);
+			sumatoria = 0;
+			stringstream floatValue(A[a][6]);
+			num = 0;
+			floatValue >> num;
+			sumatoria += num;
+			provincia = A[a][3];
+		}
 	}
-}
-printf("Lista de mayor a menor de las provincias, con respecto a su superficie afectada:\n");
-lista.mostrar();
+	printf("Lista de mayor a menor de las provincias, con respecto a su superficie afectada:\n");
+	lista.mostrar();
 }
