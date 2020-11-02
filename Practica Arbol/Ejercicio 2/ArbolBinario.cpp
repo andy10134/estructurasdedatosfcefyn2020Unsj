e54d3c9750661acd17e08fdf795d8fa758082a7e
AnaCarbajo <aniic.1701@gmail.com>
2020-11-02 18:25:27 -0300

    .

--- Practica Arbol/Ejercicio 2/ArbolBinario.cpp
+++ Practica Arbol/Ejercicio 2/ArbolBinario.cpp
@@ -7,7 +7,7 @@ private:
 public:
     ArbolBinarioB(/* args */);
     int insertar(int element, Nodo *aux);
-    int suprimir(int elemento);
+    int suprimir(int elemento, Nodo *raiz);
     Nodo *buscar(int elemento);
     int nivel();
     bool hoja(int elemento, int elemento2);
@@ -15,7 +15,7 @@ public:
     bool padre(int elemento);
     int *camino(int inicio, int destino);
     int altura(int elemento);
-    int InOrden();
+    void InOrden(Nodo *raiz);
     int PreOrden();
     int PostOrden();
     Nodo *getRaiz();
@@ -50,4 +50,38 @@ int ArbolBinarioB::insertar(int elemento, Nodo * raizS )
             insertar(elemento, raizS -> obtenerIzquierda());
         }
     }
+}
+
+int ArbolBinarioB::suprimir(int elemento, Nodo *raiz)
+{
+    if(Raiz != NULL)
+    {
+	Nodo *aux;
+	if(Raiz->obtenerItem() == elemento)
+	{
+		
+	}
+	else
+	{
+		if(Raiz->obtenerItem() > elemento)
+		{
+			suprimir(elemento, raiz->obtenerDer());
+		}
+		else
+		{
+		}
+	}
+    }else
+    {
+        cout<<"Arbol vacio, no se puede suprimir"<<endl;
+	    return(0);
+    } 
+}
+void ArbolBinarioB::InOrden(Nodo *raiz){
+	if(raiz != NULL)
+	{
+		InOrden(raiz->obtenerIzquierda());
+		cout<<raiz->obtenerItem()<<endl;
+		InOrden(raiz->obtenerDerecha());
+	}
 }
