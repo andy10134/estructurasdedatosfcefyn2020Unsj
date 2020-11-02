863f697f02b7f1f46cfae5e23525e100d0169a77
andy10134 <andy1013@hotmail.com>
2020-11-02 16:29:44 -0300

    d

--- Practica Arbol/Ejercicio 2/ArbolBinario.cpp
+++ Practica Arbol/Ejercicio 2/ArbolBinario.cpp
@@ -4,9 +4,22 @@ private:
     Nodo *Raiz;
 public:
     ArbolBinario(/* args */);
+    int insertar(int elemento);
+    int suprimir(int elemento);
+    Nodo *buscar(int elemento);
+    int nivel();
+    bool hoja(int elemento, int elemento2);
+    bool hijo(int elemento, int elemento2);
+    bool padre(int elemento);
+    int *camino(int inicio, int destino);
+    int altura(int elemento);
+    int InOrden();
+    int PreOrden();
+    int PostOrden();
 };
 
 ArbolBinario::ArbolBinario(/* args */)
 {
-    Nodo
+    Nodo = NULL;
 }
+
