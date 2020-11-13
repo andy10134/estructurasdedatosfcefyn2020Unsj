de8f56de54c912555494366423d60b9b1b88b449
andy10134 <andy1013@hotmail.com>
2020-11-13 16:47:36 -0300

    .

--- Practica Arbol/Ejercicio 7/colaPrioridad.cpp
+++ Practica Arbol/Ejercicio 7/colaPrioridad.cpp
@@ -31,14 +31,28 @@ void colaPrioridad::suprimir()
 
         int i = 1;
         int aux;
-        while (items[i] > items[i*2] || items[i] > items[i*2+1] && i < items[0])
+        while (items[i] > items[i*2] && i*2 < items[0])
         {       
-            if (items[i*2] > items[i*2+1])
+            if (items[0] >= i*2+1)
             {
+                if(items[i*2] < items[i*2+1])
+                {
+                    aux = items[i*2];
+                    items[i*2] = items[i];
+                    items[i] = aux; 
+                    i = i*2;
+                }else
+                {
+                aux = items[i*2+1];
+                items[i*2+1] = items[i];
+                items[i] = aux; 
                 i = i*2+1;
-            } else
+                }
+            }else
             {
-                i = i*2;
+                aux = items[i*2];
+                items[i*2] = items[i];
+                items[i] = aux; 
             }
             
         }
