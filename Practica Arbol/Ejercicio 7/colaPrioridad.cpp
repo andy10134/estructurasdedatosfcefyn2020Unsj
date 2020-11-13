e141e97d2d0a21c61ab7c629fcb4b6b76cd2f2b8
AnaCarbajo <aniic.1701@gmail.com>
2020-11-13 15:47:12 -0300

    .

--- Practica Arbol/Ejercicio 7/colaPrioridad.cpp
+++ Practica Arbol/Ejercicio 7/colaPrioridad.cpp
@@ -5,10 +5,28 @@ private:
     int *items;
 public:
     colaPrioridad(int cantidad);
+    void insertar(int elemento);
 };
 
 colaPrioridad::colaPrioridad(int cantidad)
 {
     tope = -1;
     items = new int[cantidad];
+}
+
+void colaPrioridad::insertar(int elemento){
+     if(tope < maximo)
+    {
+        int i= tope;
+        items[i] = elemento;
+        while(i!=1 && items[i/2] > elemento)
+        {
+            int aux;
+            aux = items[i/2];
+            items[i/2] = elemento;
+            items[i] = aux;
+            i = i/2;
+        }
+        tope++;
+    }
 }
