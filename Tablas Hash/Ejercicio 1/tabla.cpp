89ff1cc0c6f391014b63bdfa54469653c85757ab
andy10134 <andy1013@hotmail.com>
2020-11-16 17:07:15 -0300

    .

--- Tablas Hash/Ejercicio 1/tabla.cpp
+++ Tablas Hash/Ejercicio 1/tabla.cpp
@@ -24,3 +24,36 @@ tablaHash::tablaHash(int cantidad)
         items[i] = -1;
     }
 }
+
+void tablaHash::insertar(int elemento)
+{
+    int i=0, aux;
+
+    aux = elemento%max;
+
+    if (items[aux] == -1)
+    {
+        items[aux] = elemento;
+    }else
+    {   
+        int aux2 = (aux2-1)%max;
+        while (aux != aux2 && items[aux2] != -1 && items[aux2] == elemento)
+        {
+            aux2 = (aux2-1)%max;           
+        }
+        
+        if (aux2 == aux)
+        {
+            cout << "Tabla saturada" << endl;
+        }else if (items[aux2] == elemento)
+        {
+            cout << "Ya se ha insertado ese elemento" << endl;
+        }else        
+        {
+            items[aux] = elemento;
+            cout << "Exito" << endl;
+        }
+        
+        
+    }   
+}
