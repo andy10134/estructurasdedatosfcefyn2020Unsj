c11c9d63364c2b181f537e8d0be30a7dd3db2293
andy10134 <andy1013@hotmail.com>
2020-11-19 18:47:14 -0300

    .

---
+++ Tablas Hash/Ejercicio 2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "tabla.cpp"
+#include <cstdlib>
+#include <time.h>
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+        int i=0, aux, random;
+        cout << "Ingrese el numero de claves" << endl;
+        cin >> aux;
+
+        for ( i = 0; i < aux; i++)
+        {
+            random = rand()%5;
+            
+        }
+        
+
+            tablaHash tabla = tablaHash();
+}
